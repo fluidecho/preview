8325b1f85b7ba417e4881227043b7596ba7d53a6
markcode <mark@kurunt.com>
2014-05-25 17:17:05 +1000

    random colours

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var preview = require('./index')('app1');
 
-var foo = {bar: 2, hello: 'world'};
+var foo = {bar: 1, hello: 'world'};
 
 var foo2 = {bar: 2, hello: 'world', list: [1,2,3,4,5], fab: true};
 
@@ -17,3 +17,25 @@ preview('hello from app1, 3');
 preview('app2', 'hello from app2, 2, foo2', foo2);
 
 preview('hello from app1, 4');
+
+preview('maths addition', 'add 4 and 5: ' + (4 + 5));
+
+var x = 1;
+preview('x = ' + x++);
+preview('x = ' + x++);
+preview('x', 'x = ' + x++);
+
+var bar = {
+	barbar: function (b) {
+	}
+};
+
+preview('bar', 'x = ' + x++, bar);
+
+preview('hello1', 'world1');
+
+preview('hello2', 'world2');
+
+preview('hello3', 'world3');
+
+preview('hello4', 'world4');
